11c8cf34b1077f191fc720a2233dc9fae87a8172
Diego Galdamez <diegogaldamez08@gmail.com>
2022-08-09 10:42:26 -0600

    Iniciando Proyecto
    Cargando proyecto coffee shop en Laravel.

---
+++ Proyecto2_Laravel/database/migrations/2022_08_08_222429_create_clients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',50);
+            $table->string('last_name',50);
+            $table->string('identification_number',50);
+            $table->string('email',100)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('telephone_number', 20);
+            $table->string('birth_date', 20);
+            $table->string('address', 20);
+            $table->string('password', 50);
+            $table->boolean("client_state")->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
---
+++ Proyecto2_Laravel/database/migrations/2022_08_08_232720_add_foreign_key_in_order_details.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('id_product')->nullable();
+            $table->foreign('id_product')->references('id')->on('products');
+
+            $table->unsignedBigInteger('id_order')->nullable();
+            $table->foreign('id_order')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+        });
+    }
+};
